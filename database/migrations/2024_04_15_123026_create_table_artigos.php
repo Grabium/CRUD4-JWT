7dcfdb74d6c68b4f0ff84f0bcefc384a89dd5c44
Gabriel Azzevedo <110872805+Grabium@users.noreply.github.com>
2024-04-15 13:06:47 +0000

    #1 - Tabelas Artigos e Marcadores, foram criadas. Sem relação.
    A tabela Histórico foi movida para a pasta exc. por enquanto, não instauremas-na.

---
+++ database/migrations/2024_04_15_123026_create_table_artigos.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artigos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            //$table->unsignedBigInteger('marcadores_id');
+            $table->string('name')->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_artigos');
+    }
+};
---
+++ database/migrations/2024_04_15_123258_create_table_marcadores.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_marcadores', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nome', 12)->unique();
+            $table->string('descricao')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_marcadores');
+    }
+};
